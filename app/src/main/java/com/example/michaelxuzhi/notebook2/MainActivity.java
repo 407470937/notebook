975a3249db7b45366f05372429c128bb2f6d8233
BY <407470937@qq.com>
2019-05-20 21:38:22 +0800

    My notebook

---
+++ app/src/main/java/com/example/michaelxuzhi/notebook2/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.michaelxuzhi.notebook2;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.Button;
+        import android.widget.ListView;
+      //import java.util.Timer;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private ListView lv;
+    private SQLiteDatabase dbReader;
+    private Cursor cursor;
+    private NotesDB notesDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+
+    }
+
+    private void initView() {
+        Button addbtn = (Button) findViewById(R.id.main_add);
+        lv = (ListView) findViewById(R.id.main_list);
+        addbtn.setOnClickListener(this);
+        notesDB = new NotesDB(this);
+        dbReader = notesDB.getReadableDatabase();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cursor.moveToPosition(position);
+                Intent i = new Intent(MainActivity.this, SelectAct.class);
+                i.putExtra(NotesDB.ID, cursor.getInt(cursor.getColumnIndex(NotesDB.ID)));
+                i.putExtra(notesDB.CONTENT, cursor.getString(cursor.getColumnIndex(NotesDB.CONTENT)));
+                i.putExtra(notesDB.TIME, cursor.getString(cursor.getColumnIndex(NotesDB.TIME)));
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, Addcontent.class);
+        startActivity(i);
+    }
+
+    private void selectDB() {
+        cursor = dbReader.query(NotesDB.TABLE_NAME, null, null, null, null, null, null);
+        MyAdapter adapter = new MyAdapter(this, cursor);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        selectDB();
+    }
+}
+
